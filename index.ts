88b9469e4dccb45c4d94201d7b285feb51c546e7
lizraeli <leo2002b@yahoo.com>
2020-04-27 10:18:02 +0300

    export everything

---
+++ index.ts
@@ -0,0 +1,17 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import * as core from "./src/core";
+import * as array from "./src/array";
+import * as pair from "./src/pair";
+import * as threeTuple from "./src/threeTuple";
+import * as shape from "./src/shape";
+import * as types from "./src/types";
+
+export default {
+    ...core,
+    ...array,
+    ...pair,
+    ...threeTuple,
+    ...shape,
+    ...types
+}
